c64a7e6a3aea9ec0ec6d8f4b6410315d65d79c86
dol-onia <dolonia0524@gmail.com>
2025-03-11 13:08:18 -0700

    Resolved the error in previous commit, added stub implementation for subtract, multiply, divide functions, as well as ran tests, all failed as expected

--- calculator.c
+++ calculator.c
@@ -2,3 +2,15 @@
 int add(int a, int b) {
     return a + b;
 }
+
+int subtract(int a, int b) {
+    return 0;
+}
+
+int multiply(int a, int b) {
+    return 0;
+}
+
+double divide(int a, int b) {
+    return (double)(a) / (double)(b);
+}
